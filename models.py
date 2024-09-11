c2e82d84a961a4ddba7f7763895000831a1c7aa6
Beyza Kaya <105801503+beyzoskaya@users.noreply.github.com>
2024-09-12 00:10:12 +0300

    Smaller network added
    Smaller network added

--- models.py
+++ models.py
@@ -3,7 +3,9 @@ from torch.nn import Linear
 from torch import cdist
 from layers import SAGEConv
 
-class Net(torch.nn.Module):
+# GraphSAGE based model, builds upon SAGEConv layer 
+# Can be used for node classification, link prediction, graph-level prediction
+class Net(torch.nn.Module): 
   def __init__(self):
     super(Net, self).__init__()
     self.conv = SAGEConv(512, 512)
@@ -14,7 +16,7 @@ class Net(torch.nn.Module):
   
   def forward(self, x, edge_index):
     x = self.conv(x, edge_index)
-    x = x.relu()
+    x = x.relu() # non-linearity preserved for complex patterns
     x = self.densea(x)
     x = x.relu()
     x = self.dense1(x)
@@ -38,3 +40,30 @@ class Net(torch.nn.Module):
     x = self.dense3(x)
 
     return x 
+
+# Total number of parameters in this case: 697475
+
+class SmallerNet(torch.nn.Module):
+  def _init_(self):
+    super(SmallerNet, self).__init__()
+    self.conv = SAGEConv(256,256)
+    self.densea = Linear(256, 128)
+    self.dense1 = Linear(128, 64)
+    self.dense2 = Linear(64, 32)
+    self.dense3 = Linear(32, 3)
+  
+  def forward(self, x, edge_index):
+    x = self.conv(x, edge_index)
+    x = x.relu()
+    x = self.densea(x)
+    x = x.relu()
+    x = self.dense1(x)
+    x = x.relu()
+    x = self.dense2(x)
+    x = x.relu()
+    x = self.dense3(x)
+
+    return x 
+
+# Total number of parameters in this case needs to be: 435331
+
