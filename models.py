4f34d4e951526c044e4bcf7d0773b120e4c8a7ca
beyzoskaya <beyza.kaya@ozu.edu.tr>
2024-09-12 20:39:44 +0300

    GATConv versions of networks

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 from torch.nn import Linear
 from torch import cdist
 from layers import SAGEConv
+from layers import GATConv
 
 # GraphSAGE based model, builds upon SAGEConv layer 
 # Can be used for node classification, link prediction, graph-level prediction
@@ -94,3 +95,76 @@ class SmallerNet(torch.nn.Module):
     return x 
 # Total number of parameters: 305731
 
+
+### Graph Attention Network (GAT) Versions ###
+
+class GATNet(torch.nn.Module):
+    def __init__(self):
+        super(GATNet, self).__init__()
+        # CustomGATConv version (GAT)
+        self.conv = GATConv(512, 512, heads=4)
+        self.densea = Linear(512, 256)
+        self.dense1 = Linear(256, 128)
+        self.dense2 = Linear(128, 64)
+        self.dense3 = Linear(64, 3)
+
+    def forward(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        x = self.densea(x)
+        x = x.relu()
+        x = self.dense1(x)
+        x = x.relu()
+        x = self.dense2(x)
+        x = x.relu()
+        x = self.dense3(x)
+        x = cdist(x, x, p=2)
+        return x
+
+    def get_model(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        x = self.densea(x)
+        x = x.relu()
+        x = self.dense1(x)
+        x = x.relu()
+        x = self.dense2(x)
+        x = x.relu()
+        x = self.dense3(x)
+        return x
+
+
+class GATSmallerNet(torch.nn.Module):
+    def __init__(self):
+        super(GATSmallerNet, self).__init__()
+        # CustomGATConv version for smaller model (GAT)
+        self.conv = GATConv(512, 256, heads=4)
+        self.densea = Linear(256, 128)
+        self.dense1 = Linear(128, 64)
+        self.dense2 = Linear(64, 32)
+        self.dense3 = Linear(32, 3)
+
+    def forward(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        x = self.densea(x)
+        x = x.relu()
+        x = self.dense1(x)
+        x = x.relu()
+        x = self.dense2(x)
+        x = x.relu()
+        x = self.dense3(x)
+        x = cdist(x, x, p=2)
+        return x
+
+    def get_model(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        x = self.densea(x)
+        x = x.relu()
+        x = self.dense1(x)
+        x = x.relu()
+        x = self.dense2(x)
+        x = x.relu()
+        x = self.dense3(x)
+        return x
