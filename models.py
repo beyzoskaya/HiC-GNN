2b448208847df4754fc239376128395a1702dec9
beyzoskaya <beyza.kaya@ozu.edu.tr>
2024-10-07 20:48:22 +0300

    reduced model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Linear, Dropout
+from torch.nn import Linear, Dropout, BatchNorm1d
 from torch import cdist
 from layers import SAGEConv
 from layers import CustomGATConv
@@ -144,19 +144,63 @@ class GATNetReduced(torch.nn.Module):
     def __init__(self):
         super(GATNetReduced, self).__init__()
         self.conv = GATConv(512, 512, heads=2, concat=True)  # Output size becomes 1024 with concat=True
-        self.densea = Linear(1024, 128) 
-        self.dense1 = Linear(128, 64)
+        #self.batch_norm1 = BatchNorm1d(1024)
+        #self.densea = Linear(1024, 128)
+        self.densea = Linear(1024,256)
+        #self.batch_norm2 = BatchNorm1d(128) 
+        self.dense1 = Linear(256, 64)
+        #self.batch_norm3 = BatchNorm1d(64)
         self.dense2 = Linear(64, 3)
-
-        self.dropout = Dropout(p=0.3)  
+        self.dropout = Dropout(p=0.4)  
 
     def forward(self, x, edge_index):
         # GAT Layer
         x = self.conv(x, edge_index)
         x = x.relu()
+        #x = self.batch_norm1(x)
         x = self.dropout(x)
         
         # Dense layers
+        x = self.densea(x)
+        x = x.relu()
+        #x = self.batch_norm2(x)
+        x = self.dropout(x)
+        
+        x = self.dense1(x)
+        x = x.relu()
+        #x = self.batch_norm3(x)
+        x = self.dense2(x)
+        
+        x = cdist(x, x, p=2)
+        return x
+
+    def get_model(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        #x = self.batch_norm1(x)
+        x = self.densea(x)
+        x = x.relu()
+        #x = self.batch_norm2(x)
+        x = self.dense1(x)
+        x = x.relu()
+        #x = self.batch_norm3(x)
+        x = self.dense2(x)
+        return x
+  
+class GATNetMoreReduced(torch.nn.Module):
+    def __init__(self):
+        super(GATNetMoreReduced, self).__init__()
+        self.conv = GATConv(256, 256, heads=2, concat=True) 
+        self.densea = Linear(512, 128) 
+        self.dense1 = Linear(128, 32)   
+        self.dense2 = Linear(32, 3)     
+        self.dropout = Dropout(p=0.4)  
+
+    def forward(self, x, edge_index):
+        x = self.conv(x, edge_index)
+        x = x.relu()
+        x = self.dropout(x)
+        
         x = self.densea(x)
         x = x.relu()
         x = self.dropout(x)
@@ -165,7 +209,7 @@ class GATNetReduced(torch.nn.Module):
         x = x.relu()
         x = self.dense2(x)
         
-        x = cdist(x, x, p=2)
+        x = cdist(x, x, p=2)  
         return x
 
     def get_model(self, x, edge_index):
@@ -177,6 +221,7 @@ class GATNetReduced(torch.nn.Module):
         x = x.relu()
         x = self.dense2(x)
         return x
+
     
 class GATSmallerNet(torch.nn.Module): 
   def __init__(self):
@@ -234,26 +279,26 @@ class Encoder(torch.nn.Module):
         self.dropout = Dropout(p=0.3)
 
     def forward(self, x, edge_index):
-        print(f"Shape of x: {x.shape}")
+        #print(f"Shape of x: {x.shape}")
         # GATConv layer
         x = self.conv1(x, edge_index)
-        print(f"After GATConv1, x.shape: {x.shape}")
+        #print(f"After GATConv1, x.shape: {x.shape}")
         x = F.relu(x)
         x = self.dropout(x)
         
         # GCNConv layer
         x = self.conv2(x, edge_index)
-        print(f"After GCNConv2, x.shape: {x.shape}")
+        #print(f"After GCNConv2, x.shape: {x.shape}")
         x = F.relu(x)
         x = self.dropout(x)
 
         # Dense layers to latent space
         x = self.densea(x)
-        print(f"After densea, x.shape: {x.shape}")
+        #print(f"After densea, x.shape: {x.shape}")
         x = F.relu(x)
         x = self.dropout(x)
         latent = self.dense_latent(x)
-        print(f"Latent space, latent.shape: {latent.shape}")
+        #print(f"Latent space, latent.shape: {latent.shape}")
         return latent
 
 class Decoder(torch.nn.Module):
@@ -269,21 +314,21 @@ class Decoder(torch.nn.Module):
     def forward(self, z, edge_index):
         # Dense layers from latent space
         x = F.relu(self.dense1(z))
-        print(f"After dense1 in Decoder, x.shape: {x.shape}")
+        #print(f"After dense1 in Decoder, x.shape: {x.shape}")
         x = self.dropout(x)
         x = F.relu(self.dense2(x))
-        print(f"After dense2 in Decoder, x.shape: {x.shape}")
+        #print(f"After dense2 in Decoder, x.shape: {x.shape}")
         x = self.dropout(x)
         
         # GCNConv layer
         x = self.conv1(x, edge_index)
-        print(f"After GCNConv in Decoder, x.shape: {x.shape}")
+        #print(f"After GCNConv in Decoder, x.shape: {x.shape}")
         x = F.relu(x)
         x = self.dropout(x)
         
         # GATConv layer for final reconstruction
         x = self.conv2(x, edge_index)
-        print(f"After GATConv in Decoder, x.shape: {x.shape}")
+        #print(f"After GATConv in Decoder, x.shape: {x.shape}")
         return x
 
 class AutoencoderGAT_GCN(torch.nn.Module):
@@ -294,11 +339,11 @@ class AutoencoderGAT_GCN(torch.nn.Module):
 
     def forward(self, x, edge_index):
         latent = self.encoder(x, edge_index)
-        print(f"Latent representation from Encoder, latent.shape: {latent.shape}")
+        #print(f"Latent representation from Encoder, latent.shape: {latent.shape}")
         recon_x = self.decoder(latent, edge_index)
-        print(f"Reconstructed x, recon_x.shape: {recon_x.shape}")
+        #print(f"Reconstructed x, recon_x.shape: {recon_x.shape}")
         pairwise_distances = torch.cdist(recon_x, recon_x, p=2)
-        print(f"Pairwise distances, pairwise_distances.shape: {pairwise_distances.shape}")
+        #print(f"Pairwise distances, pairwise_distances.shape: {pairwise_distances.shape}")
         return pairwise_distances
 
 
