513c190659d1c170e5ec533fac92646a023fe5a0
beyzoskaya <beyza.kaya@ozu.edu.tr>
2024-10-14 22:57:43 +0300

    fine-tune other species

--- HiC-GNN_finetune.py
+++ HiC-GNN_finetune.py
@@ -1,126 +1,100 @@
 import numpy as np
-from ge import LINE 
 from node2vec import Node2Vec
 import sys
 import utils
 import networkx as nx
 import os
-from models import Net
-from models import SmallerNet
+from models import GATNetHeadsChanged4LayersLeakyReLU
 import torch
 from torch.nn import MSELoss
 from torch.optim import Adam
 from scipy.stats import spearmanr
+import random
 import ast
 import argparse
+import json
+import matplotlib.pyplot as plt
+
+torch.manual_seed(42)
+np.random.seed(42)
+random.seed(42)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+def calculate_dRMSD(truth_distances, predicted_distances):
+    squared_diff = torch.pow(truth_distances - predicted_distances, 2)
+    mean_squared_diff = torch.mean(squared_diff)
+    dRMSD = torch.sqrt(mean_squared_diff)
+    return dRMSD.item()
 
 if __name__ == "__main__":
-    if not os.path.exists('Outputs'):
-        os.makedirs('Outputs')
-    if not os.path.exists('Data'):
-        os.makedirs('Data')
-
-    parser = argparse.ArgumentParser(description='Generate embeddings and train a HiC-GNN model with transfer learning.')
+    parser = argparse.ArgumentParser(description='Fine-tune HiC-GAT model for another species.')
     
-    parser.add_argument('dataset_folder', type=str, help='Input dataset folder path containing dataset folders.')
-    parser.add_argument('subdataset', type=str, help='Sub-dataset name (e.g., CH12-LX).')
-    parser.add_argument('resolution', type=str, help='Resolution subfolder name (e.g., 1mb).')
+    parser.add_argument('dataset_folder', type=str, help='Input dataset folder path containing species data.')
+    parser.add_argument('species', type=str, help='Species name (e.g., K562 or CH12-LX).')
     parser.add_argument('chromosome', type=str, help='Chromosome name (e.g., chr12).')
-    parser.add_argument('--pretrained', type=str, help='Path to pretrained model weights for transfer learning.')
-    
-    parser.add_argument('-c', '--conversions', type=str, default='[.1,.1,2]', help='List of conversion constants.')
-    parser.add_argument('-bs', '--batchsize', type=int, default=128, help='Batch size for embeddings generation.')
-    parser.add_argument('-ep', '--epochs', type=int, default=10, help='Number of epochs used for embeddings generation.')
-    parser.add_argument('-lr', '--learningrate', type=float, default=0.001, help='Learning rate for training GCNN.')
-    parser.add_argument('-th', '--threshold', type=float, default=1e-8, help='Loss threshold for training termination.')
-    parser.add_argument('--pretrained', type=str, help='Path to pretrained model weights for transfer learning.') 
+    parser.add_argument('resolution', type=str, help='Resolution subfolder name (e.g., 1mb, 100kb).')
+    parser.add_argument('--pretrained_weights', type=str, default='path_to_gm12878_weights.pt', help='Path to pretrained GM12878 weights.')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate for fine-tuning.')  
+    parser.add_argument('--threshold', type=float, default=1e-7, help='Loss threshold for fine-tuning.')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs for fine-tuning.')
     
     args = parser.parse_args()
 
     dataset_folder = args.dataset_folder
-    subdataset = args.subdataset
-    resolution = args.resolution
-    chromosome = args.chromosome
-    
-    batch_size = args.batchsize
-    epochs = args.epochs
-    lr = args.learningrate
+    species = args.species  
+    resolution = args.resolution  
+    chromosome = args.chromosome 
+    lr = args.lr  
     thresh = args.threshold
-    conversion = 1
-    
+    pretrained_weights = args.pretrained_weights  
+    epochs = args.epochs
 
     filename = f'{chromosome}_{resolution}.RAWobserved.txt'
-    filepath = os.path.join(dataset_folder, subdataset, resolution, filename)
+    filepath = os.path.join(dataset_folder, species, resolution, filename)
     
     if not os.path.exists(filepath):
         print(f'File {filepath} not found. Please check the folder and file structure.')
         sys.exit(1)
 
-    name = f'{subdataset}_{chromosome}_{resolution}'
+    name = f'{species}_{chromosome}_{resolution}'
 
     adj = np.loadtxt(filepath)
-
+    print(f"Shape of raw data: {adj.shape}")
 
     if adj.shape[1] == 3:
-        print('Converting coordinate list format to matrix.')
         adj = utils.convert_to_matrix(adj)
-
+    
     np.fill_diagonal(adj, 0)
-    matrix_path = f'Data/{name}_matrix.txt'
+    matrix_path = f'Data/Data_{species}_fine_tune/{name}_matrix.txt'
     np.savetxt(matrix_path, adj, delimiter='\t')
 
     os.system(f'Rscript normalize.R {name}_matrix')
-    normed_matrix_path = f'Data/{name}_matrix_KR_normed.txt'
-    print(f'Created normalized matrix for {filepath} as {normed_matrix_path}')
-
+    normed_matrix_path = f'Data/Data_{species}_fine_tune/{name}_matrix_KR_normed.txt'
     normed = np.loadtxt(normed_matrix_path)
 
-    G = nx.from_numpy_matrix(adj)
-
-    # Node2vec model for creating embeddings
-    node2vec = Node2Vec(G, dimensions=512, walk_length=80, num_walks=10, workers=4)
-    model = node2vec.fit(window=10, min_count=1, batch_words=4)
-    embeddings = np.array([model.wv[str(node)] for node in G.nodes()])
-    embedding_path = f'Data/{name}_embeddings_node2vec.txt'
-    np.savetxt(embedding_path, embeddings)
-    print(f'Created embeddings corresponding to {filepath} as {embedding_path}')
-
+    G = nx.from_numpy_array(adj)
+    node2vec = Node2Vec(G, dimensions=512, walk_length=100, num_walks=20, p=2, q=0.5, workers=1, seed=42)
+    model_node2vec = node2vec.fit(window=15, min_count=1, batch_words=4)
+    embeddings = np.array([model_node2vec.wv[str(node)] for node in G.nodes()])
 
     data = utils.load_input(normed, embeddings)
 
-    tempmodels, tempspear, tempmse, model_list = [], [], [], []
-
-    model = Net()
-    print("Model Architecture:")
-    print(model)
-
-    # For pretrain (fine-tuning)
-    if args.pretrained:
-        print(f'Loading pretrained model from {args.pretrained}')
-        model.load_state_dict(torch.load(args.pretrained))
+    model = GATNetHeadsChanged4LayersLeakyReLU()
     
-    # Freeze all params
-    for param in model.parameters():
-        param.requires_grad = False
-    
-    # Unfreeze last layer (dense3 in Net model)
-    for name, param in model.named_parameters():
-        if "dense3" in name:
-            param.requires_grad = True
-            print(f"Unfreezing layer: {name}")  
-    
-    optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-5)
-
-    total_params = sum(p.numel() for p in model.parameters())
-    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f'Total number of parameters: {total_params}')
-    print(f'Total trainable parameters: {trainable_params}')
+    model.load_state_dict(torch.load(pretrained_weights))
+    model.train()
 
     criterion = MSELoss()
-    oldloss, lossdiff = 1, 1
-    truth = utils.cont2dist(data.y, conversion)
+    optimizer = Adam(model.parameters(), lr=lr)
 
-    while lossdiff > thresh:
+    oldloss, lossdiff = 1, 1
+    loss_history = []
+    truth = utils.cont2dist(data.y, 1)  
+
+    epoch = 0
+    while lossdiff > thresh and epoch < epochs:
         model.train()
         optimizer.zero_grad()
         out = model(data.x, data.edge_index)
@@ -129,24 +103,30 @@ if __name__ == "__main__":
         loss.backward()
         optimizer.step()
         oldloss = loss
-        print(f'Loss: {loss}', end='\r')
+        
+        loss_history.append(loss.item())
+        epoch += 1
+        print(f'Epoch {epoch}, Loss: {loss.item()}', end='\r')
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(loss_history, label='Loss per Epoch', color='blue')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(f'Loss Curve for {name} Fine-tuned on {species}')
+    plt.legend()
+    loss_plot_path = f'Outputs/{species}_fine_tune/{name}_loss_plot.png'
+    plt.savefig(loss_plot_path)
+    print(f'Saved fine-tuning loss plot to {loss_plot_path}')
+
+    torch.save(model.state_dict(), f'Outputs/{species}_fine_tune/{name}_fine_tuned_weights.pt')
+    print(f'Saved fine-tuned weights for {species} at {chromosome} to Outputs/{species}_fine_tune/{name}_fine_tuned_weights.pt')
 
     idx = torch.triu_indices(data.y.shape[0], data.y.shape[1], offset=1)
     dist_truth = truth[idx[0, :], idx[1, :]]
     coords = model.get_model(data.x, data.edge_index)
     dist_out = torch.cdist(coords, coords)[idx[0, :], idx[1, :]]
     SpRho = spearmanr(dist_truth, dist_out.detach().numpy())[0]
+    dRMSD_value = calculate_dRMSD(dist_truth, dist_out)
 
-    repmod, repspear, repmse = coords, SpRho, loss
-
-    print(f'Optimal dSCC: {repspear}')
-    
-    # Save fine-tuned model results
-    with open(f'Outputs/{name}_node2vec_finetuned_log.txt', 'w') as f:
-        f.writelines([f'Conversion factor: {conversion}\n', f'Optimal dSCC: {repspear}\n', f'Final MSE loss: {repmse}\n'])
-    
-    torch.save(model.state_dict(), f'Outputs/{name}_node2vec_weights_finetuned.pt')
-    utils.WritePDB(repmod * 100, f'Outputs/{name}_node2vec_structure_finetuned.pdb')
-
-    print(f'Saved fine-tuned model to Outputs/{name}_node2vec_weights_finetuned.pt')
-    print(f'Saved optimal structure to Outputs/{name}_node2vec_structure_finetuned.pdb')
+    print(f'Fine-tuned dSCC: {SpRho}')
+    print(f'Fine-tuned dRMSD: {dRMSD_value}')
