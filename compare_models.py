ff6b0c8f71449d47142e001b576469a02cce2dae
beyzoskaya <beyza.kaya@ozu.edu.tr>
2024-10-22 00:19:51 +0300

    compare ChIA-PET eval results

--- compare_models.py
+++ compare_models.py
@@ -9,7 +9,6 @@ model_folders = [
     'GATNetConvLayerChanged_lr_0.0001_GM12878_evaluation',
     'GATNetHeadsChangedLeakyReLU_lr_0.0001_epoch_500_GM12878_evaluation',
     'node2vec_GAT_reduced_overfitted_GM12878_evaluation',
-    'GATNetHeadsChanged4LayerEmbedding256_lr_0.0.001_dropout_0.3_threshold_1e-8_AdamW_GM12878_evaluation',
     'GATNetHeadsChanged4LayerEmbedding256Dense_lr_0.0.0009_dropout_0.3_threshold_1e-8_GM12878_evaluation',
     'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.0009_dropout_0.3_threshold_1e-7_GM12878_evaluation',
     'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.0008_dropout_0.3_threshold_1e-7_GM12878_evaluation',
@@ -17,13 +16,36 @@ model_folders = [
     'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.001_dropout_0.3_threshold_1e-7_node2vecParamsChanged_GM12878_evaluation',
     'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.001_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_evaluation',
     'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.002_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_evaluation',
-    'GATNetHeadsChanged4Layers_lr_0.0.0009_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_evaluation'
+    'GATNetHeadsChanged4Layers_lr_0.0.0009_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_evaluation',
+    'GATNetHeadsChanged4Layers_lr_0.0.0009_dropout_0.3_threshold_1e-8_node2vecParamsChanged_p_0.5_q_2_GM12878_evaluation',
+    'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0009_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_evaluation'
     
 ]
 
+model_ChIAPET_folders = [
+'GATNetHeadsChanged3LayersLeakyReLU_lr_0.0.0005_dropout_0.3_threshold_1e-8_p_4_q_2.5_GM12878_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged3LayersLeakyReLUv2_lr_0.0.0005_dropout_0.3_threshold_1e-8_p_4_q_2.5_GM12878_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged3LayersLeakyReLUv3_lr_0.0.0003_dropout_0.3_threshold_1e-8_p_8_q_0.75_num_walks_30_GM12878_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged3LayersLeakyReLUv3_lr_0.0.0005_dropout_0.3_threshold_1e-8_p_4_q_2.5_GM12878_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayerEmbedding512Dense_lr_0.0.0009_dropout_0.3_threshold_1e-7_node2vecParamsChanged_GM12878_ChIA-PET_evaluation',
+'GATNetHeadsChanged4Layers_lr_0.0.0007_dropout_0.3_threshold_1e-8_p_2_q_0.5_GM12878_weight_decay_1e-6_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0001_dropout_0.3_threshold_1e-8_node2vecParamsChanged_p_2_q_0.5_GM12878_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0003_dropout_0.3_threshold_1e-7_node2vecParamsChanged_p_1.5_q_0.75_GM12878_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0003_dropout_0.3_threshold_1e-8_node2vecParamsChanged_p_2_q_0.5_GM12878_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0003_dropout_0.3_threshold_1e-8_node2vecParamsChanged_p_2_q_0.5_GM12878_regularization_added_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0003_dropout_0.3_threshold_1e-8_p_6_q_2_num_walks_50_GM12878_batch_size_128_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0005_dropout_0.3_threshold_1e-8_node2vecParamsChanged_p_1.5_q_0.5_GM12878_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0005_dropout_0.3_threshold_1e-8_p_2_q_0.5_GM12878_weight_decay_1e-6_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0005_dropout_0.3_threshold_1e-8_p_4_q_2.5_GM12878_batch_size_64_ChIA-PET_evaluation',
+'GATNetHeadsChanged4LayersLeakyReLU_lr_0.0.0009_dropout_0.3_threshold_1e-8_node2vecParamsChanged_GM12878_ChIA-PET_evaluation'
+
+]
+
+
+
 comparison_results = {}
 
-for model_folder in model_folders:
+for model_folder in model_ChIAPET_folders:
     model_path = os.path.join(base_output_dir, model_folder)
     
     for file_name in os.listdir(model_path):
